c135e082dfadff25a201e841117a8319836e75d5
Samsideen-Naeem <110115318+Samsideen-Naeem@users.noreply.github.com>
2022-09-13 14:35:28 +0000

    login authorization

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react'
-import Login from './screens/Login'
-import Home from './screens/Home'
-import Register from './screens/Register'
+import React, { useState } from 'react'
+import ProfileCard from './components/ProfileCard'
+import ProfileForm  from './components/ProfileForm'
 
 function App() {
+
+
+  const [allProfile, setAllprofiles] = useState([
+
+    {
+      firstname:"Samsideen",
+      lastname: "Naeem",
+      email:"comfort@gmail.com",
+      phone:"0246183596"
+    }
+  ])
+
+const updateprofile = (profile) => {
+  let arr = allProfile;
+  arr.push(profile)
+  setAllprofiles([...arr])
+};
+
   return (
-    <div>
-      
+    <>
+    <div className='app'>
+  <h1>Our profile maker</h1>
     </div>
+    <ProfileForm submit={updateprofile} /> 
+    <hr />
+    <div className='list'>
+      {allProfile.map((person, index) =>(
+       <ProfileCard key={index} card={person} /> 
+      ))}
+
+
+
+    </div>
+    </>
   )
 }
 
